b67a76487da1c1d1bf848926218da4b46f760064
Daniel Connor <dconnor@catalystitservices.com>
2013-09-11 15:07:41 -0700

    kmain-151 - Hyphens are ignored in search.
    Signed-off-by: Daniel Connor <dconnor@catalystitservices.com>
    (cherry picked from commit 4fb48270a0266a1daec56035459f23e1f65a154e)
    
    Signed-off-by: Kyle Tomita <tomitakyle@gmail.com>

--- usr/lib/perl5/site_perl/5.8.8/OpenILS/WWW/EGCatLoader/Search.pm
+++ usr/lib/perl5/site_perl/5.8.8/OpenILS/WWW/EGCatLoader/Search.pm
@@ -35,6 +35,7 @@ sub _prepare_biblio_search_basics {
         # This stuff probably will need refined or rethought to better handle
         # the weird things Real Users will surely type in.
         $contains = "" unless defined $contains; # silence warning
+        $query =~ s/-//g; # Get rid of -
         if ($contains eq 'nocontains') {
             $query =~ s/"//g;
             $query = ('"' . $query . '"') if index $query, ' ';
